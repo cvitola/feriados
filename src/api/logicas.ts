1c608b44915ad6e7eb9fae4c659d3c494bff0563
cvitola <isrunaway@gmail.com>
2025-03-23 10:56:35 -0300

    dias proximo correjido

--- src/api/logicas.ts
+++ src/api/logicas.ts
@@ -9,7 +9,7 @@ export const diasProximoFeriado = async(): Promise<number> => {
     let anioActual = new Date().getFullYear().toString();
     let hoy = obtenerFechaISO();
     let feriados = await levantarFeriados(anioActual);
-    let sig = feriados?.filter(f => diffDays(hoy,f.fecha) < 0 )[0];
+    let sig = feriados?.filter(f => diffDays(hoy,f.fecha) <= 0 )[0];
     if (!sig) return null; // No hay próximo feriado
     return diffDays(sig.fecha,hoy)+1;
 }
